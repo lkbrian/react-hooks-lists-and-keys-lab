999263bf4ee64df6c87b2bde0c4666703da61ea7
lkbrian <lkbrian742@gmail.com>
2024-01-30 14:02:06 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map((item) => {
+        return (
+          <a key={item} href={"#" + item}>
+            {item}
+          </a>
+        );
+      })}
+    </nav>
+  );
 }
 
 export default NavBar;
--- src/data/user.js
+++ src/data/user.js
@@ -28,5 +28,5 @@ const user = {
     },
   ],
 };
-
+// export {projects}
 export default user;
